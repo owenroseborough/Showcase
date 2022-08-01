c71e7bf84e47519b13808e65e9990db30e55e86c
OWEN ROSEBOROUGH <76256902+owenroseborough@users.noreply.github.com>
2022-08-01 13:59:35 -0400

    Add files via upload

--- CPU_Info_Filtering_Script.py
+++ CPU_Info_Filtering_Script.py
@@ -8,7 +8,6 @@ def certainNumberTopProcesses(lines, num):
 
     processCounter = 0
     outputProcesses = False
-
     stringOutput = ''
 
     #look through file until we get to CPU process list
@@ -29,11 +28,41 @@ def certainNumberTopProcesses(lines, num):
     with open('cpu_info_refined.txt', 'w') as f:
         f.write(stringOutput)
 
+#function to filter by process IDs
+#first argument array of lines of txt file
+#second argument list of process IDs to output
+def processIDs(lines, listIDs):
+
+    processCounter = 0
+    outputProcesses = False
+    stringOutput = ''
+
+    #look through file until we get to CPU process list
+    for line in lines:
+        arrayLineItems = line.split()
+        if len(arrayLineItems) > 0 and arrayLineItems[0] == 'PID':
+            outputProcesses = True
+            stringOutput += line
+            processCounter = 0
+        if outputProcesses and arrayLineItems[0] in listIDs:
+            stringOutput += line
+            if processCounter == (len(listIDs) - 1):
+                stringOutput += '\n'
+                processCounter = 0
+                outputProcesses = False
+            processCounter += 1
+
+    #output stringOutput to text file
+    with open('cpu_info_refined.txt', 'w') as f:
+        f.write(stringOutput)
+
 def main():
     
     with open('cpu_info.txt') as f:
         lines = f.readlines()
-    certainNumberTopProcesses(lines, 10)
+    #certainNumberTopProcesses(lines, 10)
+    #processIDs(lines,['1964','289','531','590'])
+    
 
 if __name__ == "__main__":
     main()
